9e6ba0f8a5913a5e13ee9835696beee905b1b0e2
henriquemv23 <smeagle2003@gmail.com>
2025-06-10 09:47:35 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ function gerarFaturaStr (fatura, pecas) {
                           { style: "currency", currency: "BRL",
                             minimumFractionDigits: 2 }).format;
   // função extraída
-  function calcularTotalApresentacao(apre, peca) {
+  function calcularTotalApresentacao(apre) {
     let total = 0; 
+    const peca = getPeca(apre);
     switch (peca.tipo) {
       case "tragedia":
         total = 40000;
@@ -29,17 +30,20 @@ function gerarFaturaStr (fatura, pecas) {
     }
     return total; 
   }
-
+  // função query
+    function getPeca(apresentacao) {
+      return pecas[apresentacao.id];
+    }
   for (let apre of fatura.apresentacoes) {
-    const peca = pecas[apre.id];
-    let total = calcularTotalApresentacao(apre, peca); 
+    //const peca = pecas[apre.id];
+    let total = calcularTotalApresentacao(apre); 
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
-      if (peca.tipo === "comedia") 
+      if (getPeca(apre).tipo === "comedia") 
          creditos += Math.floor(apre.audiencia / 5);
   
       // mais uma linha da fatura
-      faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
     faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
